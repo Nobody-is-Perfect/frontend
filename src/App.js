ec40fe07b9bf4e473e325a5e90ebe628493f6d08
Samuel von Baussnern <savoba@protonmail.ch>
2020-04-19 16:50:25 +0200

    more ignored words

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Input } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+import axios from 'axios';
+import {__BASE_URL__} from './constants';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      player: {
+        name: null,
+        id: null
+      }
+    }
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleEnterPressed = this.handleEnterPressed.bind(this);
+  }
+
+  handleUsernameChange(e) {
+    this.setState({
+      player: {name: e.target.value}
+    });
+  }
+
+  handleEnterPressed(e) {
+    let name = this.state.player.name
+    if (name && name.length > 0) {
+      // TODO show loading
+      console.log('sending user name ' + name)
+      axios.post(__BASE_URL__ + '/players', {
+        name: name
+      }).then(res => {
+        console.log("got: ", res)
+        this.setState({
+          player: res
+        });
+      }).catch(err => {
+        console.debug(err);
+      });
+    } else {
+      // TODO show error
+      console.log('user name is empty');
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Input
+            placeholder="Your cool user name"
+            value={this.state.player.name}
+            onChange={this.handleUsernameChange}
+            onPressEnter={this.handleEnterPressed}
+            prefix={<UserOutlined className="site-form-item-icon" />}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const __BASE_URL__ = 'http://127.0.0.1:8080'
